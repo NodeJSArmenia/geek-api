43fc0004298d91ac1e4bd76df902080cf4b1ee73
JSHayk <jshaykdeveloper@gmail.com>
2022-10-02 20:40:08 +0400

    removed list post route

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -2,18 +2,6 @@ const getList = async (req, res) => {
   res.json([]);
 };
 
-const addListItem = async (req, res) => {
-  const { firstName, lastName, image, links, hashtags } = req.body;
-
-  res.json({
-    firstName,
-    lastName,
-    image,
-    links,
-    hashtags,
-  });
-};
-
 export default {
   getList,
   addListItem,
--- routes/list.router.js
+++ routes/list.router.js
@@ -5,6 +5,5 @@ import listController from "../controllers/list.controller.js";
 const router = express();
 
 router.get("/list", listController.getList);
-router.post("/list", listController.addListItem);
 
 export default router;
