6bad56435c4fadb6bb78615f848c984be1150fd6
JSHayk <jshaykdeveloper@gmail.com>
2022-10-08 23:50:15 +0400

    changed .env

--- geeks/geek.js
+++ geeks/geek.js
@@ -6,16 +6,17 @@ const { airtable_geeks_token } = config.airtableConfig;
 
 const getGeeks = async () => {
   const data = await client.getTableByName(airtable_geeks_token);
-  const mapedData = data.map((item) => {
-    const { Websites } = item.fields;
-    const website = getGeekWebsites(Websites);
-    return mapGeeks(item.fields, website);
-  });
-  console.log(await Promise.all(mapedData), "Maped Data"); // EveryThing is Ok here.
-  return await Promise.all(mapedData);
+  const mapedData = await Promise.all(
+    data.map(async (item) => {
+      const { Websites } = item.fields;
+      const website = await getGeekWebsites(Websites);
+      return mapGeeks(item.fields, website);
+    })
+  );
+  return mapedData;
 };
 
-export async function mapGeeks(geek, website) {
+export function mapGeeks(geek, website) {
   const { Hashtags, Name, Avatar } = geek;
 
   return {
